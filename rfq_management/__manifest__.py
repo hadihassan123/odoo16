28e992bdfcfc256c76b3c9bb72c63cd552aaf40b
hadi <hadihassan.aa123@gmail.com>
2024-08-02 10:40:37 +0530

    rfq

---
+++ rfq_management/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    'name': 'Sales Purchase Updates',
+    'author' : 'Abdulhadi',
+    'website' : 'https://tctenterprise.com/',
+    'category' : 'Sales Purchase Udpates',
+    'summary' : 'Sales Price Update from Purchase Module.',
+    'depends' : ['mail','sale_management','hr','sale','maintenance','base',],
+    'data' : [
+        'security/ir.model.access.csv',
+        'data/product_price_template.xml',
+        'views/product_summary.xml',
+        'views/sale_summary.xml',
+
+        # 'views/css.xml',
+
+
+    ],
+    'demo' :[],
+    'qweb' :[],
+    'assets': {
+        'web.assets_backend': [
+            # 'amc_management/static/src/css/custom_styles.css',
+            # 'amc_management/static/src/components/**/*.js',
+            # 'amc_management/static/src/components/**/*.xml',
+            # 'amc_management/static/src/components/**/*.scss'
+        ],
+
+    },
+
+
+
+}
